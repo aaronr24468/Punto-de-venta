78e04968a371c3d3eea82ed7584ea10da99bd9e1
aaronr24468 <aaronr24468@gmail.com>
2026-04-16 19:52:52 -0600

    improvement in the main interface

--- src/hooks/useMain.js
+++ src/hooks/useMain.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import * as ScreenOrientation from "expo-screen-orientation";
 import { BackHandler } from "react-native";
 import SystemNavigationBar from "react-native-system-navigation-bar";
@@ -7,7 +7,10 @@ import SystemNavigationBar from "react-native-system-navigation-bar";
 export const useMain = () =>{
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
-    const [list, setList] = useState([])
+    const [list, setList] = useState([]);
+    const [searchI, SetSearchI] = useState(false)
+
+    const inputSearch = useRef();
 
     const getProductsList = async() =>{
         
@@ -18,6 +21,8 @@ export const useMain = () =>{
     }, [])
 
     return{
-        getProductsList
+        getProductsList,
+        SetSearchI,
+        searchI
     }
 }
--- src/styles/mainStyles.js
+++ src/styles/mainStyles.js
@@ -5,7 +5,119 @@ export const style = StyleSheet.create({
     mainComponent: {
         flex: 1,
         flexDirection: 'column',
-        flexWrap: 'nowrap'
+        flexWrap: 'nowrap',
+        position: 'relative',
+        paddingBottom: 50,
+        backgroundColor: 'white'
     },
+    options: {
+        position: 'absolute',
+        right: 20,
+        top: 20,
+        width: 40,
+        height: 40,
+        zIndex: 10
+
+    },
+    optionsSvg: {
+        width: '100%',
+        height: "100%",
+        objectFit: 'cover'
+    },
+    totalComponent: {
+        width: '100%',
+        height: '150',
+        boxShadow: '0px 3px 10px 0px rgb(0,0,0)'
+    },
+    totalAmountContainer: {
+        width: '100%',
+        height: '150',
+        padding: 20,
+        justifyContent: 'center',
+    },
+    textAmount: {
+        fontSize: 50
+    },
+
+
+    scrollList: {
+        width: '100%',
+        height: '100%',
+        position: "relative",
+        
+    },
+    searchMainContainer: {
+        width: '100%',
+        height: 'auto',
+        position: 'absolute',
+        top: 0,
+        zIndex: 10
+    },
+    searchContainer: {
+        width: '100%',
+        height: 'auto',
+        position: 'relative'
+    },
+    inputContainer: {
+        width: '100%',
+        height: 'auto',
+        position: 'absolute',
+        zIndex: 100,
+        backgroundColor: 'white',
+        boxShadow: '0px 0px 5px 0px rgb(0,0,0)',
+    },
+    inputSearchContainer: {
+        width: '100%',
+        height: 'auto',
+        position: 'relative'
+    },
+    inputSearchItem: {
+        width: '100%',
+        height: 60,
+        fontSize: 20,
+        paddingRight: 20,
+        paddingLeft: 20
+    },
+    exitSeachItem: {
+        width: 30,
+        height: 30,
+        position: 'absolute',
+        right: 15,
+        top: 15,
+        boxShadow: '0px 3px 10px 0px rgb(0,0,0)',
+        borderRadius: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    exitBtn: {
+        width: '80%',
+        height: '80%'
+    },
+    searchBtn: {
+        width: 30,
+        height: 30,
+        position: 'absolute',
+        right: 15,
+        top: 15,
+        boxShadow: '0px 3px 10px 0px rgb(0,0,0)',
+        borderRadius: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 10
+    },
+    imageSearch: {
+        width: '80%',
+        height: '80%'
+    },
+
+
+    sellFinish: {
+        width: '100%',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        boxShadow: '0px 0px 5px 0px rgb(0,0,0)',
+        backgroundColor: 'rgba(81, 255, 0, 0.5)'
+    }
 
 })
